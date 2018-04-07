a81f0d2032c06d325479707f0b6738606a55478d
Faisal Ashraf <faisalshafeeqashraf@gmail.com>
2018-04-07 14:22:47 +0100

    refactor

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -9,27 +9,32 @@ namespace StringCalculatorKata
 
         public int Add(string numberString)
         {
-            List<char> defaultDelimiters = new List<char>{ ',', '\n' };
-
-            var delimiterString= numberString.IndexOf("//", StringComparison.Ordinal);
-            if (delimiterString > -1)
-            {
-                var delimiter = numberString.Substring(delimiterString+2,1);
-              
-                numberString = numberString.Replace("//" + delimiter + "\n", "");
-                defaultDelimiters.Add(delimiter[0]);
-            }
+            List<char> defaultDelimiters = new List<char> { ',', '\n' };
+          
+            defaultDelimiters.Add(GetAdditionalDelimiters(ref numberString));
 
             if (string.IsNullOrEmpty(numberString)) return 0;
 
-            var splitString = numberString.Split(defaultDelimiters.ToArray(),StringSplitOptions.RemoveEmptyEntries);
+            var splitString = numberString.Split(defaultDelimiters.ToArray()
+                , StringSplitOptions.RemoveEmptyEntries);
+
             var total = CalculateSum(splitString);
             return total;
         }
 
-        private static int CalculateSum(string[] numbers)
+        private static int CalculateSum(IEnumerable<string> numbers)
         {
             return numbers.Select(i => int.Parse(i)).Sum();
+        }
+
+        public char GetAdditionalDelimiters(ref string numberString)
+        {
+            var delimiterString = numberString.IndexOf("//", StringComparison.Ordinal);
+            if (delimiterString <= -1) return char.MinValue;
+            var delimiter = numberString.Substring(delimiterString + 2, 1);
+
+            numberString = numberString.Replace("//" + delimiter + "\n", "");
+            return delimiter[0];
         }
     }
 }
